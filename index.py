bbea9c80b719f2e1c605f10c27cdd750a64a9502
William Téllez <williametelleza@gmail.com>
2023-07-26 16:53:35 -0400

    ver 0.002: limpieza de comentarios y líneas de más

--- index.py
+++ index.py
@@ -1,23 +1,17 @@
-from flask import Flask, render_template
-# from flask import make_response 
-
-# cursor = <connection-name>.cursor(buffered=True)
+from flask import Flask
 
 app = Flask(__name__)
-# app = APIFlask(__name__)
 
 @app.route('/')
 
+# Función raíz
 def home():
     return 'home'
-
-# Función raíz
 
 # Función about, una subruta en la página.
 @app.route('/about')
 def about():
     return 'About'
-
 
 if __name__ == '__main__':
     app.run()
